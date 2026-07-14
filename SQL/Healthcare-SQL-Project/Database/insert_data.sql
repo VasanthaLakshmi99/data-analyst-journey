a98227fef34262a0923bea81e148f80d22ee6cd7
VasanthaThotireddy <vasanthathotireddy99@gmail.com>
2026-07-14 13:57:41 +0530

    Create insert_data.sql

---
+++ SQL/Healthcare-SQL-Project/Database/insert_data.sql
@@ -0,0 +1,7 @@
+-- =====================================
+-- Healthcare SQL Project
+-- Insert Data Script
+-- PostgreSQL
+-- =====================================
+
+-- Data import commands will be added here.
