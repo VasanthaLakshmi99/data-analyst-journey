be92ebda708be29ba210d45234127ca1405cfc1b
VasanthaThotireddy <vasanthathotireddy99@gmail.com>
2026-07-14 13:48:11 +0530

    Create create_tables.sql

---
+++ SQL/Healthcare-SQL-Project/Database/create_tables.sql
@@ -0,0 +1,6 @@
+-- =====================================
+-- Healthcare SQL Project
+-- PostgreSQL Database Schema
+-- =====================================
+
+-- Tables will be added here during Day 1.
