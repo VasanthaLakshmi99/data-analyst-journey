435920147b939ca0abf73e8495bd034cecffe756
VasanthaThotireddy <vasanthathotireddy99@gmail.com>
2026-07-17 13:44:18 +0530

    Update 05_HAVING.sql

--- SQL/Healthcare-SQL-Project/SQL Queries/05_HAVING.sql
+++ SQL/Healthcare-SQL-Project/SQL Queries/05_HAVING.sql
@@ -1,1 +1,64 @@
-
+---->The Hospital Director wants to identify insurance providers having more than 100 registered patients.
+---->Finance wants to identify payment methods whose total revenue exceeds ₹5,00,000.
+---->HR wants to identify specializations having more than 10 doctors.
+---->Operations wants to identify appointment statuses having more than 200 appointments.
+---->Medical Department wants to identify treatment types whose average treatment cost exceeds ₹25,000.
+---->Finance wants to identify patients whose total bill amount exceeds ₹1,00,000.
+---->Operations wants to identify doctors who completed more than 50 appointments.
+---->The Hospital Director wants to identify specializations whose average doctor experience exceeds 12 years.
+---->Finance wants to identify payment methods that satisfy BOTH:More than 100 transactions,Revenue greater than ₹20,00,000
+---->Operations wants to identify appointment dates having more than 30 appointments.
+---->The CEO wants to identify insurance providers satisfying ALL conditions:
+More than 500 patients
+Average treatment cost above ₹30,000
+Maximum treatment cost above ₹2,00,000
+---->Finance wants payment methods where:
+Revenue > ₹50,00,000
+Average bill > ₹10,000
+Highest bill > ₹1,00,000
+---->HR wants specializations where:
+More than 15 doctors
+Average experience >10 years
+Minimum experience >3 years
+---->Operations wants doctors who satisfy:
+More than 100 completed appointments
+Earliest appointment before 2024-01-01
+Latest appointment after 2024-12-31
+---->Medical Department wants treatment types where:
+More than 300 treatments
+Average cost >₹20,000
+Highest cost >₹1,50,000
+Lowest cost >₹5,000
+---->The CEO wants only hospital branches satisfying ALL conditions:
+Completed appointments only
+More than 1000 completed appointments
+Revenue greater than ₹2 crore
+Average billing above ₹25,000
+Highest bill above ₹2 lakh
+---->Identify insurance providers satisfying:
+More than 1000 patients
+Revenue above ₹5 crore
+Average bill above ₹30,000
+Minimum bill above ₹5000
+Highest bill above ₹3 lakh
+---->Display all KPI columns.
+Identify doctors satisfying ALL conditions:
+Completed appointments only
+More than 250 completed appointments
+Average treatment cost above ₹40,000
+Highest treatment cost above ₹2 lakh
+Earliest appointment before 2023-01-01
+---->Identify doctors satisfying ALL conditions:
+Completed appointments only
+More than 250 completed appointments
+Average treatment cost above ₹40,000
+Highest treatment cost above ₹2 lakh
+Earliest appointment before 2023-01-01
+---->The Hospital Board wants to identify high-performing departments.
+Conditions:
+Only Completed appointments
+More than 500 completed appointments
+Average treatment cost above ₹30,000
+Total billing above ₹3 crore
+Highest treatment cost above ₹2 lakh
+Minimum billing amount above ₹1000
